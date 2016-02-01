0b17d5f9433ce987a3c52b7d6f8c5dbcc96379ab
ltjax <marius.elvert@googlemail.com>
2016-02-01 23:53:11 +0100

    Starting implementation work

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
-
+#include <vector>
+#include <string>
 #include <raknet/RakPeerInterface.h>
 #include <raknet/NatPunchthroughServer.h>
 
 int main()
 {
+    auto MaximumConnections = 128;
+
     RakNet::RakPeerInterface* rakPeer=RakNet::RakPeerInterface::GetInstance();
+
+    /*std::vector<std::string> addressList;
+    for (int i=0, ie=rakPeer->GetNumberOfAddresses(); i!=ie; ++i)
+    {
+        auto IP = rakPeer->GetLocalIP(i);
+        if (IP!=RakNet::UNASSIGNED_SYSTEM_ADDRESS)
+            addressList.push_back(IP);
+    }*/
+
+    RakNet::NatPunchthroughServer natPunchthroughServer;
+    rakPeer->AttachPlugin(&natPunchthroughServer);
+
+    std::vector<RakNet::SocketDescriptor> socketDescriptorList = {
+        RakNet::SocketDescriptor(9000, "")
+    };
+
+    rakPeer->Startup(MaximumConnections, socketDescriptorList.data(), socketDescriptorList.size());
     rakPeer->Shutdown(100);
+
     RakNet::RakPeerInterface::DestroyInstance(rakPeer);
 
     return 0;
