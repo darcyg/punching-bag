f3b1fd6e1c1a14d178f21c5d22afdaee32d2fe1d
ltjax <marius.elvert@googlemail.com>
2016-02-06 01:45:13 +0100

    Using CmdParser and implemented main-loop and sigterm handling

--- main.cpp
+++ main.cpp
@@ -2,24 +2,41 @@
 #include <string>
 #include <raknet/RakPeerInterface.h>
 #include <raknet/NatPunchthroughServer.h>
-#include <cxxopts.hpp>
+#include <cmdparser.hpp>
+#include <signal.h>
 
 namespace {
-
-auto const title = "punching-bag";
-
+volatile sig_atomic_t stop_running = 0;
 }
 
-int main(int argn, char** argv)
+void sigterm_handler(int)
 {
-    cxxopts::Options options(title);
-    options.add_options()
-            ("p,port", "Port to listen on");
+    stop_running = 1;
+}
 
+void register_sigterm()
+{
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = sigterm_handler;
+    sigaction(SIGTERM, &action, NULL);
 
-    options.parse(argn, argv);
+    std::cout << "Registering SIGTERM handler" << std::endl;
+}
 
+void run(int argn, char** argv)
+{
+    cli::Parser parser(argn, argv);
+    parser.set_required<unsigned short>("p", "port", "Port to listen on");
+
+    parser.run_and_exit_if_error();
+
+    register_sigterm();
+
+    auto port = parser.get<unsigned short>("p");
     auto MaximumConnections = 128;
+
+    std::cout << "Starting server on port " << port << std::endl;
 
     RakNet::RakPeerInterface* rakPeer=RakNet::RakPeerInterface::GetInstance();
 
@@ -27,13 +44,36 @@ int main(int argn, char** argv)
     rakPeer->AttachPlugin(&natPunchthroughServer);
 
     std::vector<RakNet::SocketDescriptor> socketDescriptorList = {
-        RakNet::SocketDescriptor(options["port"].as<unsigned short>(), "")};
+        RakNet::SocketDescriptor(port, "")};
 
     rakPeer->Startup(MaximumConnections, socketDescriptorList.data(), socketDescriptorList.size());
+
+    std::cout << "Server started" << std::endl;
+
+    while(!stop_running)
+    {
+        sleep(1);
+    }
+
+    std::cout << "Stopping.." << std::endl;
+
     rakPeer->Shutdown(100);
 
     RakNet::RakPeerInterface::DestroyInstance(rakPeer);
 
-    return 0;
+    std::cout << "Server shut down!" << std::endl;
 }
 
+int main(int argn, char** argv)
+{
+    try
+    {
+        run(argn, argv);
+        return 0;
+    }
+    catch(std::exception const& error)
+    {
+        std::cout << error.what() << std::endl;
+        return -1;
+    }
+}
